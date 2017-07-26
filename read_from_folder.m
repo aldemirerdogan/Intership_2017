229ee49c02e0bd19cb33a0ab58dcb2ee10fec8b2
Erdogan Aldemir <aldemir.erdogan@gmail.com>
2017-07-26 15:32:59 +0300

    Read from folder
    read *.mat data file, you must be the current folder that includes the *.mat file

---
+++ read_from_folder.m
@@ -0,0 +1,14 @@
+% read data from folder
+
+clear ALL;
+clc;
+close all;
+slice_number =97;
+liverp_v = zeros(512,512,slice_number);
+
+% slice_number =100;
+for a = 1:slice_number
+   filename = ['liverp_1' num2str(a-1,'%02d')];
+   img = imread(filename);
+   liverp_v(:,:,a) = imbinarize(img);
+end
